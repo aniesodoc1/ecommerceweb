d2574190a72c5878ca9a16f9ff234fed6b1750b5
Aniesodo collins <aniesodoc@gmail.com>
2025-05-10 19:46:20 +0100

    last

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import Pin from './Pin';
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const Map = ({ data }) => {
   const center = [6.5841, 3.9860]; // Epe, Lagos
