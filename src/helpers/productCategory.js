271f986dd97d5309429507ec145180de5a7febd0
Aniesodo collins <aniesodoc@gmail.com>
2025-05-10 19:18:08 +0100

    13 commit

--- src/helpers/productCategory.js
+++ src/helpers/productCategory.js
@@ -1,9 +1,16 @@
 const productCategory = [
-    { id : 1, label : "Health", value : "health"},
-    { id : 2, label : "Beauty", value : "beauty"},
-    { id : 3, label : "Fashion", value : "fashion"},
-    { id : 4, label : "Aphrodisiac", value : "aphrodisiac"},
-    { id : 5, label : "Sexualwellness", value : "sexualwellness"}
+{ id : 1, label : "Airpodes", value : "airpodes"},
+    { id : 2, label : "Camera", value : "camera"},
+    { id : 3, label : "Earphones", value : "earphones"},
+    { id : 4, label : "Mobiles", value : "mobiles"},
+    { id : 5, label : "Mouse", value : "Mouse"},
+    { id : 6, label : "Printers", value : "printers"},
+    { id : 7, label : "Processor", value : "processor"},
+    { id : 8, label : "Refrigerator", value : "refrigerator"},
+    { id : 9, label : "Speakers", value : "speakers"},
+    { id : 10, label : "Trimmers", value : "trimmers"},
+    { id : 11, label : "Televisions", value : "televisions"},
+    { id : 12, label : "Watches", value : "watches"},
 ]
 
 export default productCategory
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,11 +9,17 @@ const Home = () => {
     <div>
       <CategoryList/>
       <BannerProduct/>
-      <HorizontalCardProduct category={"health"} heading={"Top's health drugs"}/>
-      <HorizontalCardProduct category={"beauty"} heading={"Popular beauties"}/> 
-      <VerticalCardProduct category={"fashion"}heading={"Popular fashion"} />
-      <VerticalCardProduct category={"aphrodisiac"} heading={"aphrodisiac"}/>
-      <VerticalCardProduct category={"sexualwellness"} heading={"Sexual Wellness"}/>
+       <HorizontalCardProduct category={"airpodes"} heading={"Top's Airpodes"}/>
+      <HorizontalCardProduct category={"watches"} heading={"Popular's Watches"}/>
+
+      <VerticalCardProduct category={"mobiles"} heading={"Mobiles"}/>
+      <VerticalCardProduct category={"Mouse"} heading={"Mouse"}/>
+      <VerticalCardProduct category={"televisions"} heading={"Televisions"}/>
+      <VerticalCardProduct category={"camera"} heading={"Camera & Photography"}/>
+      <VerticalCardProduct category={"earphones"} heading={"Wired Earphones"}/>
+      <VerticalCardProduct category={"speakers"} heading={"Bluetooth Speakers"}/>
+      <VerticalCardProduct category={"refrigerator"} heading={"Refrigerator"}/>
+      <VerticalCardProduct category={"trimmers"} heading={"Trimmers"}/>
       </div>
   )
 }
